ad96125510e1e3d57072857c5e9f786508ac9fa8
BURAKTONOZ <buraktonoz@gmail.com>
2026-09-17 23:18:31 +0300

    Fix syntax error in preload.js

--- preload.js
+++ preload.js
@@ -1,6 +1,5 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
-// HTML dosyamızın içine "window.api" adında güvenli bir köprü açıyoruz
 contextBridge.exposeInMainWorld('api', {
   getData: () => ipcRenderer.invoke('get-data'),
   savePersonnel: (data) => ipcRenderer.invoke('save-personnel', data),
