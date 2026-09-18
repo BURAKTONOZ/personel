e78a2781ce23609e7d7336981b50134fd761c5b6
BURAKTONOZ <buraktonoz@gmail.com>
2026-09-18 06:42:24 +0300

    Enhance preload.js with additional IPC methods
    Added new IPC methods for database connection and window controls.

--- preload.js
+++ preload.js
@@ -3,5 +3,13 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('api', {
   getData: () => ipcRenderer.invoke('get-data'),
   savePersonnel: (data) => ipcRenderer.invoke('save-personnel', data),
-  saveSettings: (data) => ipcRenderer.invoke('save-settings', data)
+  saveSettings: (data) => ipcRenderer.invoke('save-settings', data), // Varsa
+  
+  // Yeni eklenenler
+  checkDatabase: () => ipcRenderer.invoke('check-db-connection'),
+  selectFolder: () => ipcRenderer.invoke('select-folder'),
+  setCustomDbPath: (path) => ipcRenderer.invoke('set-custom-db-path', path),
+  getDbStatus: () => ipcRenderer.invoke('get-db-status'),
+  windowMinimize: () => ipcRenderer.send('window-minimize'),
+  windowClose: () => ipcRenderer.send('window-close')
 });
