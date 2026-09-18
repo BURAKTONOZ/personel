52fae273b0239f86d552ce2f88b41542e255d2ed
BURAKTONOZ <buraktonoz@gmail.com>
2026-09-18 07:07:35 +0300

    Add window control functions to preload.js

--- preload.js
+++ preload.js
@@ -3,13 +3,14 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('api', {
   getData: () => ipcRenderer.invoke('get-data'),
   savePersonnel: (data) => ipcRenderer.invoke('save-personnel', data),
-  saveSettings: (data) => ipcRenderer.invoke('save-settings', data), // Varsa
+  saveSettings: (data) => ipcRenderer.invoke('save-settings', data),
   
-  // Yeni eklenenler
   checkDatabase: () => ipcRenderer.invoke('check-db-connection'),
   selectFolder: () => ipcRenderer.invoke('select-folder'),
   setCustomDbPath: (path) => ipcRenderer.invoke('set-custom-db-path', path),
   getDbStatus: () => ipcRenderer.invoke('get-db-status'),
+  
   windowMinimize: () => ipcRenderer.send('window-minimize'),
-  windowClose: () => ipcRenderer.send('window-close')
+  windowClose: () => ipcRenderer.send('window-close'),
+  maximizeWindow: () => ipcRenderer.send('maximize-window') // YENİ EKLENDİ
 });
