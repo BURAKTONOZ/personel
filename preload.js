9549e2d719fbfcee70fe9e6cc18ffbaaa161d13b
BURAKTONOZ <buraktonoz@gmail.com>
2026-09-20 23:39:43 +0300

    Reorganize API methods in preload.js

--- preload.js
+++ preload.js
@@ -1,18 +1,16 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('api', {
-  getData: () => ipcRenderer.invoke('get-data'),
-  savePersonnel: (data) => ipcRenderer.invoke('save-personnel', data),
-  saveSettings: (data) => ipcRenderer.invoke('save-settings', data),
-  
-  checkDatabase: () => ipcRenderer.invoke('check-db-connection'),
-  selectFolder: () => ipcRenderer.invoke('select-folder'),
-  setCustomDbPath: (path) => ipcRenderer.invoke('set-custom-db-path', path),
-  getDbStatus: () => ipcRenderer.invoke('get-db-status'),
-  backupDatabase: () => ipcRenderer.invoke('backup-database'),
-  silentBackup: (backupPath) => ipcRenderer.invoke('silent-backup', backupPath),
-  windowMinimize: () => ipcRenderer.send('window-minimize'),
-  windowMaximizeToggle: () => ipcRenderer.send('window-maximize-toggle'),
-  windowClose: () => ipcRenderer.send('window-close'),
-  maximizeWindow: () => ipcRenderer.send('maximize-window')
+    checkDatabase: () => ipcRenderer.invoke('check-db-connection'),
+    selectFolder: () => ipcRenderer.invoke('select-folder'),
+    setCustomDbPath: (path) => ipcRenderer.invoke('set-custom-db-path', path),
+    getDbStatus: () => ipcRenderer.invoke('get-db-status'),
+    getData: () => ipcRenderer.invoke('get-data'),
+    savePersonnel: (data) => ipcRenderer.invoke('save-personnel', data),
+    saveSettings: (settings) => ipcRenderer.invoke('save-settings', settings),
+    backupDatabase: () => ipcRenderer.invoke('backup-database'),
+    windowMinimize: () => ipcRenderer.send('window-minimize'),
+    windowMaximizeToggle: () => ipcRenderer.send('window-maximize-toggle'),
+    windowClose: () => ipcRenderer.send('window-close'),
+    maximizeWindow: () => ipcRenderer.send('maximize-window')
 });
